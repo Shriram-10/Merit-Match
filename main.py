cd77caad35b90675270ca7a6156b4f79f75529de
Shriram Umapathy <140259073+Shriram-10@users.noreply.github.com>
2024-07-24 16:01:06 +0530

    complete backend for normal mode

--- main.py
+++ main.py
@@ -108,13 +108,19 @@ async def create_post(user_id: int, post: PostBase, db: db_dependency):
 
 @app.get("/posts/get_posted_tasks/{user_id}")
 async def get_sent_tasks(user_id: int, db: db_dependency):
-    posted_tasks = db.query(models.Post).filter((user_id == models.Post.user_id) & (models.Post.active == True)).all()
+    posted_tasks = db.query(models.Post).filter((user_id == models.Post.user_id) & (models.Post.active == True) & (models.Post.completed == False)).all()
 
     return {"tasks" : posted_tasks, "code" : 1}
+
+@app.get("/posts/get_submitted_tasks/{user_id}")
+async def get_submitted_tasks(user_id: int, db: db_dependency):
+    submitted_tasks = db.query(models.Post).filter((user_id == models.Post.reserved) & (models.Post.active == True) & (models.Post.completed == True) & (models.Post.payment == False)).all()
+
+    return {"tasks" : submitted_tasks, "code" : 1}
     
 @app.get("/posts/get_reserved_tasks/{user_id}")
-async def get_sent_tasks(user_id: int, db: db_dependency):
-    reserved_tasks = db.query(models.Post).filter(user_id == models.Post.reserved).all()
+async def get_reserved_tasks(user_id: int, db: db_dependency):
+    reserved_tasks = db.query(models.Post).filter((user_id == models.Post.reserved) & (models.Post.completed == False)).all()
 
     return {"tasks" : reserved_tasks, "code" : 1}
 
@@ -122,6 +128,12 @@ async def get_sent_tasks(user_id: int, db: db_dependency):
 async def get_all_tasks(user_id: int, db: db_dependency):
     all_tasks = db.query(models.Post).filter((user_id != models.Post.user_id) & (models.Post.reserved == 0)).all()
     return {"tasks" : all_tasks, "code" : 1}
+
+@app.get("/posts/get_waiting_tasks/{user_id}")
+async def get_waiting_tasks(user_id: int, db: db_dependency):
+    waiting_tasks = db.query(models.Post).filter((True == models.Post.completed) & (models.Post.user_id == user_id) & (models.Post.payment == False) & (models.Post.active == True)).all()
+
+    return {"tasks" : waiting_tasks, "code" : 1}
 
 @app.post("/posts/reserve_task/{user_id}/{task_id}")
 async def reserve_task(user_id: int, task_id: int, db: db_dependency):
@@ -161,6 +173,18 @@ async def submit_task(task_id: int, user_id: int, db: db_dependency):
 
     if get_task:
         setattr(get_task, 'completed', True)
+        db.commit()
+        return {"code" : 1}
+    else:
+        return {"code" : -1}
+    
+
+@app.post("/posts/unsubmit_task/{user_id}/{task_id}")
+async def submit_task(task_id: int, user_id: int, db: db_dependency):
+    get_task = db.query(models.Post).filter((models.Post.reserved == user_id) & (models.Post.id == task_id) & (models.Post.completed == True)).first()
+
+    if get_task:
+        setattr(get_task, 'completed', False)
         db.commit()
         return {"code" : 1}
     else:
@@ -236,19 +260,10 @@ async def modify_post(user_id : int, task_id : int, db : db_dependency, modified
     else:
         return {"code" : -1}
     
-@app.get("/posts/notify_payment/{user_id}/{task_id}")
-async def send_notif(user_id: int, task_id: int, db: db_dependency):
-    accepted_task = db.query(models.Post).filter((models.Post.id == task_id) & (models.Post.reserved == user_id) & (models.Post.completed == True) & (models.Post.payment == True))
-    if accepted_task:
-        return {"code" : 1}
+@app.get("/users/get_balance/{user_id}")
+async def get_balance(user_id: int, db: db_dependency):
+    user = db.query(models.User).filter(models.User.id == user_id).first()
+    if user:
+        return {"balance" : user.karma_points, "code" : 1}
     else:
-        return {"code" : -1}
-    
-@app.get("/posts/notify_declined_submission/{user_id}/{task_id}")
-async def send_notif_declined(user_id: int, task_id: int, db: db_dependency):
-    declined_task = db.query(models.Post).filter((models.Post.id == task_id) & (models.Post.reserved == 0) & (models.Post.completed == False) & (models.Post.payment == False))
-    if declined_task:
-        return {"code" : 1}
-    else:
-        return {"code" : -1}
-    
+        return {"balance" : -1, "code" : -1}
